70c3cefb593282322c674e656ac249f6bf13da12
macroholms <mazivand@yandex.ru>
2025-06-05 02:41:22 +0600

    first commit

---
+++ app/src/main/java/com/example/breesapp/activities/SplashScreen.java
@@ -0,0 +1,44 @@
+package com.example.breesapp.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.breesapp.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+/*        SharedPreferences prefs = getSharedPreferences("settings", Context.MODE_PRIVATE);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= null;
+                if (prefs.getAll().isEmpty()){
+                    intent = new Intent(SplashScreen.this,
+                            OnboardingActivity.class);
+                }
+                else if(prefs.getAll().get("firstStart").equals("Login")) {
+                    intent = new Intent(SplashScreen.this,
+                            Login.class);
+                }
+                else if (prefs.getAll().get("firstStart").equals("Authorized")) {
+                    intent = new Intent(SplashScreen.this,
+                            MainActivity.class);
+                }
+
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);*/
+    }
+}
